1560282301f4cfbd3a3bffa3adf471cddc915bbd
Davor Telisman <davortelisman@gmail.com>
2023-08-27 12:19:00 +0200

    create download file function for are input file: button need to be on page display.html

--- app_scanning_files/read_file.py
+++ app_scanning_files/read_file.py
@@ -1,7 +1,8 @@
 import pandas as pd
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request,send_file
 
 app = Flask(__name__)
+
 
 # Function to calculate statistics about the DataFrame
 def calculate_statistics(df, file_type):
@@ -17,16 +18,30 @@ def calculate_statistics(df, file_type):
     unique_values = df.nunique()
     # Get column data preview
     data_preview = df.head()
-    # Get column correlation matrix
-    # correlation_matrix = df.corr()
     # Get data number (number of rows)
     data_number = len(df)
     # Get NaN (missing value) count
     nan_count = df.isna().sum().sum()
     # Get row number
     row_number = df.shape[0]
-    # Return the calculated statistics
-    return file_type, column_number, data_number, nan_count, row_number,data_preview,unique_values,numeric_stats,data_types,column_names
+
+    # Create a formatted summary text
+    summary = f"File Type: {file_type}\n"
+    summary += f"Number of Columns: {column_number}\n"
+    summary += f"Number of Rows: {row_number}\n"
+    summary += f"Number of Data Points: {data_number}\n"
+    summary += f"Number of Missing Values: {nan_count}\n"
+
+    summary += "\nData Types:\n"
+    for column_name, data_type in data_types.items():
+        summary += f"{column_name}: {data_type}\n"
+
+    summary += "\nUnique Values for Non-Numeric Columns:\n"
+    for column_name, unique_value_count in unique_values.items():
+        summary += f"{column_name}: {unique_value_count}\n"
+
+    return file_type, column_number, data_number, nan_count, row_number, data_preview, unique_values, numeric_stats, data_types, column_names, summary
+
 
 # HTML form for file upload
 @app.route('/', methods=['GET', 'POST'])
@@ -45,9 +60,13 @@ def upload_file():
         else:
             return "Invalid file type. Please choose from CSV, Excel, or TXT."
 
-        # Calculate statistics about the DataFrame
-        file_type, column_number, data_number, nan_count, row_number, data_preview, unique_values, numeric_stats, data_types, column_names = calculate_statistics(
+        # Calculate statistics about the DataFrame and get the summary
+        file_type, column_number, data_number, nan_count, row_number, data_preview, unique_values, numeric_stats, data_types, column_names, summary = calculate_statistics(
             df, file_type)
+
+        # Store the summary in a text file
+        with open("data_summary.txt", "w") as text_file:
+            text_file.write(summary)
 
         return render_template('display.html',
                                file_type=file_type,
@@ -62,6 +81,10 @@ def upload_file():
                                df=df)  # Pass the DataFrame to the template
 
     return render_template('upload.html')
+
+@app.route('/download_summary')
+def download_summary():
+    return send_file("data_summary.txt", as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
